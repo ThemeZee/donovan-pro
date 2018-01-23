69c45af1c1ed286100a413e464a363faff566add
Thomas W <kontakt@themecoder.de>
2018-01-23 17:56:29 +0100

    added live preview for color settings in Customizer

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -66,100 +66,106 @@
 		} );
 	} );
 
-	/* Link & Button Color Option */
+	/* Link Color Option */
 	wp.customize( 'donnager_theme_options[link_color]', function( value ) {
 		value.bind( function( newval ) {
-			var custom_css, text_color;
+			document.documentElement.style.setProperty( '--link-color', newval );
+		} );
+	} );
 
-			custom_css = 'a:link, a:visited, .infinite-scroll #infinite-handle span, .top-navigation-toggle:hover, .top-navigation-toggle:active, .top-navigation-menu a:hover, .top-navigation-menu a:active, .footer-navigation-menu a:hover, .footer-navigation-menu a:active { color: ' + newval + '; }';
-			custom_css += 'a:hover, a:focus, a:active, .infinite-scroll #infinite-handle span:hover { color: #353535; }';
-			custom_css += '.top-navigation-toggle:hover .icon, .top-navigation-toggle:active .icon, .top-navigation-menu > .menu-item-has-children a .sub-menu-icon:hover .icon, .top-navigation-menu > .menu-item-has-children a .sub-menu-icon:active .icon { fill: ' + newval + '; }';
-			custom_css += 'button, input[type="button"], input[type="reset"], input[type="submit"], .search-form .search-submit, .tzwb-tabbed-content .tzwb-tabnavi li a:hover, .tzwb-tabbed-content .tzwb-tabnavi li a:active, .tzwb-tabbed-content .tzwb-tabnavi li a.current-tab, .tzwb-social-icons .social-icons-menu li a, .scroll-to-top-button, .scroll-to-top-button:focus, .scroll-to-top-button:active { background: ' + newval + '; }';
-			custom_css += '@media only screen and (min-width: 55em) { .top-navigation-menu > .menu-item-has-children a:hover .sub-menu-icon .icon { fill: ' + newval + '; } }';
+	/* Button Color Option */
+	wp.customize( 'donnager_theme_options[button_color]', function( value ) {
+		value.bind( function( newval ) {
+			var text_color;
 
 			if( isColorLight( newval ) ) {
-				text_color = '#222222';
+				text_color = '#202020';
 			} else {
 				text_color = '#ffffff';
 			}
 
-			custom_css += 'button, input[type="button"], input[type="reset"], input[type="submit"], .tzwb-tabbed-content .tzwb-tabnavi li a:hover, .tzwb-tabbed-content .tzwb-tabnavi li a:active, .tzwb-tabbed-content .tzwb-tabnavi li a.current-tab { color: ' + text_color + '; }';
-			custom_css += '.search-form .search-submit .icon-search, .scroll-to-top-button .icon, .tzwb-social-icons .social-icons-menu li a .icon { fill: ' + text_color + '; }';
-
-			custom_css += 'button:hover, input[type="button"]:hover, input[type="reset"]:hover, input[type="submit"]:hover, button:focus, input[type="button"]:focus, input[type="reset"]:focus, input[type="submit"]:focus, button:active, input[type="button"]:active, input[type="reset"]:active, input[type="submit"]:active { color: #ffffff; }';
-			custom_css += '.search-form .search-submit:hover .icon-search, .scroll-to-top-button:hover .icon, .tzwb-social-icons .social-icons-menu li a:hover .icon { fill: #ffffff; }';
-
-			addColorStyles( custom_css, 1 );
-
-			custom_css = '.widget-title a:hover, .widget-title a:active { color: ' + newval + '; }';
-
-			addColorStyles( custom_css, 7 );
+			document.documentElement.style.setProperty( '--button-text-color', text_color );
+			document.documentElement.style.setProperty( '--button-color', newval );
 		} );
 	} );
 
-	/* Navigation Primary Color Option */
+	/* Button Hover Color Option */
+	wp.customize( 'donnager_theme_options[button_hover_color]', function( value ) {
+		value.bind( function( newval ) {
+			var text_color;
+
+			if( isColorLight( newval ) ) {
+				text_color = '#202020';
+			} else {
+				text_color = '#ffffff';
+			}
+
+			document.documentElement.style.setProperty( '--button-hover-color', newval );
+			document.documentElement.style.setProperty( '--button-hover-text-color', text_color );
+		} );
+	} );
+
+	/* Navi Color Option */
 	wp.customize( 'donnager_theme_options[navi_color]', function( value ) {
 		value.bind( function( newval ) {
-			var custom_css;
+			var text_color, text_hover_color;
 
-			custom_css = '.main-navigation-toggle, .main-navigation-toggle:focus, .main-navigation-menu, .main-navigation-menu a:link, .main-navigation-menu a:visited { color: ' + newval + '; }';
-			custom_css += '.primary-navigation-wrap, .main-navigation-menu, .main-navigation-menu ul, .main-navigation-menu ul a, .footer-content { border-color: ' + newval + '; }';
-			custom_css += '.main-navigation-toggle .icon, .main-navigation-menu > .menu-item-has-children a .sub-menu-icon .icon, .header-search .header-search-icon .icon-search, .header-search .header-search-form-wrap .header-search-form .header-search-close .icon-close { fill: ' + newval + '; }';
+			if( isColorLight( newval ) ) {
+				text_color = '#202020';
+				text_hover_color = 'rgba(0, 0, 0, 0.5)';
+			} else {
+				text_color = '#ffffff';
+				text_hover_color = 'rgba(255, 255, 255, 0.5)';
+			}
 
-			custom_css += '.main-navigation-toggle:hover, .main-navigation-toggle:active, .main-navigation-menu a:hover, .main-navigation-menu a:active { color: #3377bb; }';
-			custom_css += '.main-navigation-toggle:hover .icon, .main-navigation-toggle:active .icon, .main-navigation-menu > .menu-item-has-children a .sub-menu-icon:hover .icon, .main-navigation-menu > .menu-item-has-children a .sub-menu-icon:active .icon { fill: #3377bb; }';
-
-			addColorStyles( custom_css, 2 );
+			document.documentElement.style.setProperty( '--navi-color', newval );
+			document.documentElement.style.setProperty( '--navi-text-color', text_color );
+			document.documentElement.style.setProperty( '--navi-hover-text-color', text_hover_color );
 		} );
 	} );
 
-	/* Navigation Secondary Color Option */
-	wp.customize( 'donnager_theme_options[navi_hover_color]', function( value ) {
+	/* Submenu Color Option */
+	wp.customize( 'donnager_theme_options[navi_submenu_color]', function( value ) {
 		value.bind( function( newval ) {
-			var custom_css;
+			var text_color, text_hover_color;
 
-			custom_css = '.main-navigation-toggle:hover, .main-navigation-toggle:active, .main-navigation-menu a:hover, .main-navigation-menu a:active { color: ' + newval + '; }';
-			custom_css += '.main-navigation-toggle:hover .icon, .main-navigation-toggle:active .icon, .main-navigation-menu > .menu-item-has-children a .sub-menu-icon:hover .icon, .main-navigation-menu > .menu-item-has-children a .sub-menu-icon:active .icon, .header-search .header-search-icon:hover .icon-search, .header-search .header-search-icon:active .icon-search, .header-search .header-search-form-wrap .header-search-form .header-search-close:hover .icon-close, .header-search .header-search-form-wrap .header-search-form .header-search-close:active .icon-close { fill: ' + newval + '; }';
-			custom_css += '@media only screen and (min-width: 55em) { .main-navigation-menu > .menu-item-has-children a:hover .sub-menu-icon .icon { fill: ' + newval + '; } }';
+			if( isColorLight( newval ) ) {
+				text_color = '#202020';
+				text_hover_color = 'rgba(0, 0, 0, 0.5)';
+			} else {
+				text_color = '#ffffff';
+				text_hover_color = 'rgba(255, 255, 255, 0.5)';
+			}
 
-			addColorStyles( custom_css, 3 );
+			document.documentElement.style.setProperty( '--submenu-color', newval );
+			document.documentElement.style.setProperty( '--submenu-text-color', text_color );
+			document.documentElement.style.setProperty( '--submenu-hover-text-color', text_hover_color );
 		} );
 	} );
 
 	/* Title Color Option */
 	wp.customize( 'donnager_theme_options[title_color]', function( value ) {
 		value.bind( function( newval ) {
-			var custom_css;
-
-			custom_css = '.site-title, .site-title a:link, .site-title a:visited, .entry-title, .entry-title a:link, .entry-title a:visited { color: ' + newval + '; }';
-			custom_css += '.site-title a:hover, .site-title a:active, .entry-title a:hover, .entry-title a:active { color: #3377bb; }';
-			custom_css += '.donnager-social-menu .social-icons-menu li a .icon { fill: ' + newval + '; }';
-
-			addColorStyles( custom_css, 4 );
+			document.documentElement.style.setProperty( '--title-color', newval );
 		} );
 	} );
 
-	/* Title Hover Color Option */
-	wp.customize( 'donnager_theme_options[title_hover_color]', function( value ) {
+	/* Footer Color Option */
+	wp.customize( 'donnager_theme_options[footer_color]', function( value ) {
 		value.bind( function( newval ) {
-			var custom_css;
+			var text_color, text_hover_color;
 
-			custom_css = '.site-title a:hover, .site-title a:active, .entry-title a:hover, .entry-title a:active { color: ' + newval + '; }';
-			custom_css += '.donnager-social-menu .social-icons-menu li a:hover .icon { fill: ' + newval + '; }';
+			if( isColorLight( newval ) ) {
+				text_color = '#202020';
+				text_hover_color = 'rgba(0, 0, 0, 0.5)';
+			} else {
+				text_color = '#ffffff';
+				text_hover_color = 'rgba(255, 255, 255, 0.5)';
+			}
 
-			addColorStyles( custom_css, 5 );
-		} );
-	} );
-
-	/* Widget Title Color Option */
-	wp.customize( 'donnager_theme_options[widget_title_color]', function( value ) {
-		value.bind( function( newval ) {
-			var custom_css;
-
-			custom_css = '.widget-title, .widget-title a:link, .widget-title a:visited, .archive-title, .comments-title, .comment-reply-title, .entry-author .author-heading .author-title { color: ' + newval + '; }';
-			custom_css += '.widget-title a:hover, .widget-title a:active { color: #3377bb; }';
-
-			addColorStyles( custom_css, 6 );
+			document.documentElement.style.setProperty( '--footer-color', newval );
+			document.documentElement.style.setProperty( '--footer-text-color', text_color );
+			document.documentElement.style.setProperty( '--footer-hover-text-color', text_hover_color );
 		} );
 	} );
 
@@ -292,22 +298,6 @@
 
 	function isColorDark( hexColor ) {
 		return ( getColorBrightness( hexColor ) <= 130 );
-	}
-
-	function writeColorStyles() {
-		for( var i = 1; i < 8; i++) {
-			$( 'head' ).append( '<style id="donnager-pro-colors-' + i + '"></style>' );
-		}
-	}
-
-	function addColorStyles( colorRules, priority ) {
-
-		// Write Color Styles if they do not exist.
-		if ( ! $( '#donnager-pro-colors-1' ).length ) {
-			writeColorStyles();
-		}
-
-		$( '#donnager-pro-colors-' + priority ).html( colorRules );
 	}
 
 } )( jQuery );
